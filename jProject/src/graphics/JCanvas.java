c22781f4aa2ea7956f16ebd0e8f3fa1f945e6754
Szabo Zoltan <szabozoltanbors@gmail.com>
2013-05-10 20:30:11 +0300

    Beraktam a skalazast, gorgesd az egeret

--- jProject/src/graphics/JCanvas.java
+++ jProject/src/graphics/JCanvas.java
@@ -3,8 +3,15 @@ package graphics;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.renderable.RenderableImage;
+
 import javax.swing.JPanel;
 
 public class JCanvas extends JPanel {
@@ -12,33 +19,78 @@ public class JCanvas extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private int width;
 	private int height;
-	private float scale;
+	private double offsetX;
+	private double offsetY;
+	private double scale;
+	private BufferedImage img;
+	private Graphics gr;
+	private Graphics2D gr2D;
+	private AffineTransform transform;
 	
 	public JCanvas() {
 		width=700;
 		height=600;
-		scale = 6;
-		this.setPreferredSize(new Dimension(width,height));
-		this.setBackground(Color.DARK_GRAY);
+		scale = 1.0f;
+		transform = new AffineTransform();
+		
+		setPreferredSize(new Dimension(width,height));
 		
 		this.addMouseWheelListener(new MouseWheelListener() {
 			
 			@Override
 			public void mouseWheelMoved(MouseWheelEvent e) {
 				float mod = e.getUnitsToScroll() / 3;
-				if ((scale + mod < 12) && (scale + mod > 1)) {
+				mod = mod / 10;
+				
+				if (((scale + mod) <= 12.0f) && ((scale + mod) >= 1.0f)) {
 					scale += mod;
-					//scale(universe,scale);
+				    offsetX = e.getX() * (1.0 - scale) / scale;
+				    offsetY = e.getY() * (1.0 - scale) / scale;					
+					offsetX = (offsetX < 0) ? offsetX : 0;
+					offsetY = (offsetY < 0) ? offsetY : 0;
+					
+					repaint();
 				}
 			}
 		});
 	}
 	
-	@Override 
-	public void paint(Graphics g) {
-		super.paintComponent(g);
+	public void init() {
+		img = new BufferedImage(getWidth(),getHeight(),BufferedImage.TYPE_INT_ARGB);
+		gr = img.getGraphics();
 		
-		g.setColor(Color.WHITE);
-		g.drawLine(0,0,40,40);
+		gr.setColor(Color.DARK_GRAY);
+		gr.fillRect(0, 0,width,height);
+		
+		gr.setColor(Color.gray);
+		for (int i=0; i<width; i=i+10) {
+			gr.drawLine(i,0,i,height);
+		}
+		
+		for (int j=0; j<width; j=j+10) {
+			gr.drawLine(0,j,width,j);
+		}		
+		
+		gr.setColor(Color.BLUE);
+		gr.drawLine(width/2, 0, width/2, height);
+		
+		gr.setColor(Color.RED);
+		gr.drawLine(0, height/2, width, height/2);		
+	}
+		
+	
+	@Override 
+	public void paint(Graphics g) {				
+		if (img == null) {
+			init();
+		}	
+		
+		Graphics2D  gr2D = (Graphics2D) g;
+		transform = gr2D.getTransform();
+		transform.scale(scale,scale);
+		transform.translate(offsetX,offsetY);
+		gr2D.setTransform(transform);
+		
+		gr2D.drawRenderedImage(img, transform);
 	}	
 }
